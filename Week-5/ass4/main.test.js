f88f72b8c1a83bdf92d1dda27703e379f7827e99
Deepti Gatne <deeptigatne@gmil.com>
2022-09-24 23:15:37 +0530

    reverting

--- Week-5/ass4/main.test.js
+++
@@ -1,9 +0,0 @@
-const functions = require('./functions.js');
-
-test('adds properly', () => {
-    expect(functions.sum(1, 5)).toBe(6);
-});
-
-test('subtracts properly', () => {
-    expect(functions.subtract(3, 2)).toBe(1);
-});
